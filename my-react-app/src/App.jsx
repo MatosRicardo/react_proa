cc615268c45f59c8f67bd8bc13cdeaa5d2caa3ea
MatosRicardo <ricardoribeirodematos@gmail.com>
2024-10-01 22:56:26 -0300

    Andamento
    Mexendo nos componentes, CSS, promps e etc..

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Componentes/Header";
+import Footer from "./Componentes/Footer";
+import Food from "./Componentes/Food";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
